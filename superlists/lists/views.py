b09dbc24ca9ba439a94d2547d83c299c033f5ee8
gzpgg3x <gzpgg3x@yahoo.com>
2015-04-13 17:20:22 -0400

    priorChapter6
    priorChapter6

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # # def index(request):
 # #     render "Hello world"
@@ -17,12 +17,28 @@ from lists.models import Item
 #         return HttpResponse(request.POST['item_text'])
 #     return render(request, 'home.html')
 
-def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
-    # return render(request, 'home.html' {new_item_text: 'request.POST['item_text']'})
-    # return render(request, 'home.html', {'new_item_text': request.POST['item_text'],})
-    # return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),}) 
-    return render(request, 'home.html', {'new_item_text': item.text,}) 
+# def home_page(request):
+#     item = Item()
+#     item.text = request.POST.get('item_text', '')
+#     item.save()
+#     # return render(request, 'home.html' {new_item_text: 'request.POST['item_text']'})
+#     # return render(request, 'home.html', {'new_item_text': request.POST['item_text'],})
+#     # return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),}) 
+#     return render(request, 'home.html', {'new_item_text': item.text,})
 
+# def home_page(request):
+#     if request.method == 'POST':
+#         new_item_text = request.POST['item_text']
+#         Item.objects.create(text=new_item_text)     
+#     else:
+#         new_item_text = ''
+
+#     return render(request, 'home.html', {'new_item_text': new_item_text,})
+
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+    # return ('/')
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items}) 
