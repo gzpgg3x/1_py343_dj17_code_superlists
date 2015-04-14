1f4efc8db2fc3341932eb3761d64c3202cbc71be
gzpgg3x <gzpgg3x@yahoo.com>
2015-04-14 11:19:04 -0400

    priorChp6AdjustingOurModels
    Adjusting Our Models

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -44,18 +44,19 @@ from lists.models import Item
 #     return render(request, 'home.html', {'items': items})
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-    # return ('/')
-        return redirect('/lists/the-only-list-in-the-world/')
-    # items = Item.objects.all()
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    # # return ('/')
+    #     return redirect('/lists/the-only-list-in-the-world/')
+    # # items = Item.objects.all()
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items}) 
 
-def new_list(requests):
-    Item.objects.create(test=request.POST['text_item'])
+def new_list(request):
+    # Item.objects.create(text=request.POST['item_text'])
+    Item.objects.create(text=request.POST['item_text'])
     # return render(request, '', {'items': items})  
     return redirect('/lists/the-only-list-in-the-world/')  
